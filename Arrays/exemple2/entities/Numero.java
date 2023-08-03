c09e8521a079873d864d3e0691fc7b1a6aef1254
Carol Fagundes <131725921+carolfagundess@users.noreply.github.com>
2023-08-03 19:25:28 -0300

    Create Numero.java

---
+++ Arrays/exemple2/entities/Numero.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Numero {
+	private int num;
+
+	public Numero(int num) {
+		this.num = num;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
+	
+}
