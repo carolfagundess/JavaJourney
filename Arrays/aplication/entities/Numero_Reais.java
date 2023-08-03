5e128f463ed34ba3cd729193d38765418689718b
Carol Fagundes <131725921+carolfagundess@users.noreply.github.com>
2023-08-03 19:27:13 -0300

    Create Numero_Reais.java

---
+++ Arrays/aplication/entities/Numero_Reais.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Numeros_Reais {
+	private double numerosReais;
+
+	public Numeros_Reais(double numerosReais) {
+		super();
+		this.numerosReais = numerosReais;
+	}
+
+	public double getNumerosReais() {
+		return numerosReais;
+	}
+
+	public void setNumerosReais(double numerosReais) {
+		this.numerosReais = numerosReais;
+	} 
+	
+}
